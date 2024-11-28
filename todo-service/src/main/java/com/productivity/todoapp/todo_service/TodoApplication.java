59c17a44c7aa32cc65778d452e430603367f8997
IamVk009 <imvk009@gmail.com>
2024-11-28 19:44:40 +0530

    Todo Service

---
+++ todo-service/src/main/java/com/productivity/todoapp/todo_service/TodoApplication.java
@@ -0,0 +1,13 @@
+package com.productivity.todoapp.todo_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoApplication.class, args);
+	}
+
+}
