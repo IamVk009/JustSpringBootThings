51cfef69a09c811371fcffe878aec88f72f29576
IamVk009 <imvk009@gmail.com>
2025-01-21 23:30:00 +0530

    Chat App :  Removing Lombok Config for now

--- chat-app-backend/src/main/java/com/app/chat/entity/Message.java
+++ chat-app-backend/src/main/java/com/app/chat/entity/Message.java
@@ -1,23 +1,45 @@
 package com.app.chat.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import org.springframework.data.mongodb.core.mapping.Document;
-
 import java.time.LocalDateTime;
 
-@Document(collection = "messages")
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
 public class Message {
 
     private String sender;
     private String messageContent;
     private LocalDateTime timeStamp;
+
+    public Message() {
+    }
+
+    public Message(String sender, String messageContent, LocalDateTime timeStamp) {
+        this.sender = sender;
+        this.messageContent = messageContent;
+        this.timeStamp = timeStamp;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public void setMessageContent(String messageContent) {
+        this.messageContent = messageContent;
+    }
+
+    public void setTimeStamp(LocalDateTime timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public String getMessageContent() {
+        return messageContent;
+    }
+
+    public LocalDateTime getTimeStamp() {
+        return timeStamp;
+    }
 
     public Message(String sender, String messageContent) {
         this.sender = sender;
--- chat-app-backend/src/main/java/com/app/chat/entity/Room.java
+++ chat-app-backend/src/main/java/com/app/chat/entity/Room.java
@@ -1,9 +1,5 @@
 package com.app.chat.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -11,13 +7,42 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Document(collection = "rooms")
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
 public class Room {
     @Id
     private String id;
     private String roomId;
-    List<Message> messages = new ArrayList<>();
+    private List<Message> messages = new ArrayList<>();
+
+    public Room() {
+    }
+
+    public Room(String id, String roomId, List<Message> messages) {
+        this.id = id;
+        this.roomId = roomId;
+        this.messages = messages;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setRoomId(String roomId) {
+        this.roomId = roomId;
+    }
+
+    public void setMessages(List<Message> messages) {
+        this.messages = messages;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getRoomId() {
+        return roomId;
+    }
+
+    public List<Message> getMessages() {
+        return messages;
+    }
 }
