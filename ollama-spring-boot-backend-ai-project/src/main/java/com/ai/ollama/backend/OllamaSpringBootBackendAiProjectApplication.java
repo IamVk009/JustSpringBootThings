0e13462c70530dfd217f357c626ac563dd923149
IamVk009 <imvk009@gmail.com>
2025-02-01 06:37:58 +0530

    Spring Boot AI project using Deepseek:r1 locally with Ollama

---
+++ ollama-spring-boot-backend-ai-project/src/main/java/com/ai/ollama/backend/OllamaSpringBootBackendAiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ai.ollama.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OllamaSpringBootBackendAiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OllamaSpringBootBackendAiProjectApplication.class, args);
+	}
+
+}
---
+++ ollama-spring-boot-backend-ai-project/src/test/java/com/ai/ollama/backend/OllamaSpringBootBackendAiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai.ollama.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OllamaSpringBootBackendAiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
