d795c825d28daa5421aec6814f3869f8f3b7aea0
IamVk009 <imvk009@gmail.com>
2025-05-06 17:03:38 +0530

    Spring Boot APP : Consume REST API

---
+++ rest-api-calls/src/main/java/com/call/api/rest/RestApiCallsApplication.java
@@ -0,0 +1,13 @@
+package com.call.api.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiCallsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiCallsApplication.class, args);
+	}
+
+}
---
+++ rest-api-calls/src/test/java/com/call/api/rest/RestApiCallsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.call.api.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiCallsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
